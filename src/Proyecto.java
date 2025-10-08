0a5ee943fd30b4c59148552d303b4fa01144e394
Tomas Robino <tomaserobino@gmail.com>
2025-10-08 10:17:12 +0200

    now shows os name

--- src/Proyecto.java
+++ src/Proyecto.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 /**
  * Ejecuta los argumentos como si fuesen comandos de powershell
@@ -15,6 +16,9 @@ import java.io.InputStreamReader;
  */
 public class Proyecto {
     public static void main(String[] args) {
+        String so = System.getProperty("os.name");
+        System.out.println("Este programa se esta ejecutando en "+so);
+
         ProcessBuilder processBuilder = new ProcessBuilder();
         processBuilder.command("powershell", String.join(";", args));
         try {
