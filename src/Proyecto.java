a46d2d3e58509df6e730b2a2c9cf7a66a5176979
Tomas Robino <tomaserobino@gmail.com>
2025-10-08 10:17:28 +0200

    deleted unused import

--- src/Proyecto.java
+++ src/Proyecto.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
 
 /**
  * Ejecuta los argumentos como si fuesen comandos de powershell
